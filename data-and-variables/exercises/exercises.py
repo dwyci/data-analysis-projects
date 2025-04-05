219dbe58676f57c0b6b9145a5df88acb615b36e2
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2025-04-05 14:36:45 -0500

    calculations work

--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -1,7 +1,27 @@
 # 1. Declare and assign the variables here:
+shuttleName = "Determination"
+shuttleSpeed = 17500
+distanceToMars = 225000000
+distanceToMoon = 384400
+milesPerKm = 0.621
 
 # 2. Use print() to print the 'type' each variable. Print one item per line.
+print(type(shuttleName))
+print(type(shuttleSpeed))
+print(type(distanceToMars))
+print(type(distanceToMoon))
+print(type(milesPerKm))
 
 # Code your solution to exercises 3 and 4 here:
+milesToMars = milesPerKm*distanceToMars
+hoursToMars = milesToMars/shuttleSpeed
+daysToMars = hoursToMars/24
+
+print(shuttleName + " will take "  + str(daysToMars)+ " days to reach Mars.")
 
 # Code your solution to exercise 5 here
+milestoMoon = milesPerKm*distanceToMoon
+hoursToMoon = milestoMoon/shuttleSpeed
+daysToMoon = hoursToMoon/24
+
+print(shuttleName + " will take " + str(daysToMoon) + " days to reach the Moon")
