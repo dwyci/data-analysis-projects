f979aa04c89a765e2470f17b4e27bb36ac481f16
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2025-04-13 18:34:45 -0500

    number 5 works

--- booleans-and-conditionals/exercises03.py
+++ booleans-and-conditionals/exercises03.py
@@ -7,15 +7,27 @@ engine_temperature = 1200
 
 # a) If fuel_level is above 20000 AND engine_temperature is at or below 2500, print "Full tank. Engines good."
 
+if fuel_level > 20000 and engine_temperature <= 2500:
+    print("Full tank.  Engines good.")
+
 # b) If fuel_level is above 10000 AND engine_temperature is at or below 2500, print "Fuel level above 50%.  Engines good."
+elif fuel_level > 10000 and engine_temperature <= 2500:
+    print("Fuel level above 50%.  Engines good.")
+
 
 # c) If fuel_level is above 5000 AND engine_temperature is at or below 2500, print "Fuel level above 25%. Engines good."
+elif fuel_level > 5000 and engine_temperature <= 2500:
+    print("Fuel level above 25%.  Engines good.")
 
 # d) If fuel_level is at or below 5000 OR engine_temperature is above 2500, print "Check fuel level. Engines running hot."
-
+elif fuel_level <= 5000 and engine_temperature > 2500:
+    print("Check fuel level. Engines running hot")
 # e) If fuel_level is below 1000 OR engine_temperature is above 3500 OR engine_indicator_light is red blinking print "ENGINE FAILURE IMMINENT!" 
-
+elif fuel_level < 1000 and engine_temperature > 3500 or engine_indicator_light == 'red blinking':
+    print("ENGINE FAILURE IMMINENT!")
 # f) Otherwise, print "Fuel and engine status pending..." 
+else: 
+    print("Fuel and engine status pending...")
 
 # Code 5a - 5f here:
 
