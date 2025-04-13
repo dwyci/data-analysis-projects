1c507a20c5c467e8dd898ee9a43bedf8b7e4b73e
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2025-04-13 18:48:38 -0500

    #6 is complete

--- booleans-and-conditionals/exercises03.py
+++ booleans-and-conditionals/exercises03.py
@@ -33,10 +33,16 @@ else:
 
 
 
-# 6) a) Create the variable command_override, and set it to be true or false. If command_override is false, then the shuttle should only launch if the fuel and engine check are OK. If command_override is true, then the shuttle will launch regardless of the fuel and engine status.
-
+# 6) a) Create the variable command_override, and set it to be true or false. 
+# If command_override is false, then the shuttle should only launch if the fuel and engine check are OK. 
+# If command_override is true, then the shuttle will launch regardless of the fuel and engine status.
+command_override = False
 
 # 6) b) Code the following if/else check:
 
-# If fuel_level is above 20000 AND engine_indicator_light is NOT red blinking OR command_override is true print "Cleared to launch!" Else print "Launch scrubbed!"
-
+# If fuel_level is above 20000 AND engine_indicator_light is NOT red blinking OR command_override is 
+# true print "Cleared to launch!" Else print "Launch scrubbed!"
+if (fuel_level > 20000 and engine_indicator_light != 'red blinking') or command_override == True:
+    print("Launch scrubbed!")
+else: 
+    print("Lets git it on")
